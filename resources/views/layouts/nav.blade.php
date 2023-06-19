8c51ed5dc45edac500ef92312502b1b35708fca9
irfanfajru <irfanfajru025@gmail.com>
2023-06-19 19:50:21 +0700

    notification page created

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -18,12 +18,27 @@
             </a>
         </li>
         <li class="mr-2">
-            <a class="flex justify-center items-center inline-block border border-white hover:border-header text-black hover:bg-header py-1 px-3" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" height="0.875em" viewBox="0 0 384 512" class="mr-1 mb-1"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                    <path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 408c0 13.3-10.7 24-24 24s-24-10.7-24-24V305.9l-31 31c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l72-72c9.4-9.4 24.6-9.4 33.9 0l72 72c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-31-31V408z" />
-                </svg>
-                Pengajuan
-            </a>
+            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <a class="flex justify-center items-center inline-block border border-white hover:border-header text-black hover:bg-header py-1 px-3" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="0.875em" viewBox="0 0 384 512" class="mr-1 mb-1"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 408c0 13.3-10.7 24-24 24s-24-10.7-24-24V305.9l-31 31c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l72-72c9.4-9.4 24.6-9.4 33.9 0l72 72c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-31-31V408z" />
+                            </svg>
+                            Pengajuan
+                        </a>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('user.pengajuan')">
+                            {{ __('Pengjuan Judul') }}
+                        </x-dropdown-link>
+                        <x-dropdown-link :href="route('user.pengajuan')">
+                            {{ __('Pengjuan Seminar Proposal') }}
+                        </x-dropdown-link>
+                    </x-slot>
+                </x-dropdown>
+            </div>
         </li>
         <li class="mr-2">
             <a class="flex justify-center items-center inline-block border border-white hover:border-header text-black hover:bg-header py-1 px-3" href="{{route('user.notification')}}">

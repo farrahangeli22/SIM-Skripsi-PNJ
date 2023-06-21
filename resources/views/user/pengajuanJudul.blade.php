a5e65483e22d1c2055415d7e0c67557eb557ed6d
niyaraarinda3 <niyaraarinda3@gmail.com>
2023-06-21 21:08:52 +0700

    add main page logbook

--- resources/views/user/pengajuanJudul.blade.php
+++ resources/views/user/pengajuanJudul.blade.php
@@ -71,7 +71,7 @@
                 </div>
                 <div class="mb-4">         
                     <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deskripsi/Abstrak</label>
-                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tuliskan deskripsi skripsi anda..."></textarea>
+                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tuliskan deskripsi skripsi anda..."></textarea>
                 </div>
                 <div class="flex justify-end">
                     <x-primary-button class="flex justify-center w-fit">

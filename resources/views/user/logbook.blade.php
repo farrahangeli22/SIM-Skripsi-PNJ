8ef29b121590b65b00f58735ed33eaaddf1f6471
niyaraarinda3 <niyaraarinda3@gmail.com>
2023-06-21 11:35:18 +0700

    test logbook

--- resources/views/user/logbook.blade.php
+++ resources/views/user/logbook.blade.php
@@ -1,4 +1,21 @@
 <x-user-layout :title="'Logbook'">
-    ini logbook
+<div class="p-8 w-1/4 bg-gray-100 text-center mt-11 flex flex-col items-center">
+  <form>
+    <h1 class="text-xl">Dosen Pembimbing</h1>
+  </form>
+  
+  <div class="w-20 h-20 mr-5 mt-5 rounded-full flex items-center justify-center overflow-hidden">
+    <img class="w-full h-full object-cover" src="/assets/gambar.jpg" alt="Gambar">
+  </div>
+
+  <h4 class="mt-4 text-sm">Eriya, S.Kom., M.T.</h4>
+  <h4 class="text-sm">NIP. 197802032005012002</h4>
+</div>
+
+
+
+<div class="p-8 w-1/4 bg-gray-100 text-center mt-11">
+          <form>
+                        <h3>Dosen Pembimbing</h3> <br> </form></div> 
 
 </x-user-layout>

966a486613a5399de82b51f749956c0162b6e0ef
Farrah Dillah  Angeli <farrahdillahangeli@gmail.com>
2023-07-03 23:38:20 +0700

    fix bug modal

--- resources/views/dosen/detailLogbook.blade.php
+++ resources/views/dosen/detailLogbook.blade.php
@@ -48,7 +48,7 @@
         </div>
 
         <!-- Main modal -->
-        <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed inset-0 top-0 left-0 right-0 z-50 hidden w-full overflow-x-hidden overflow-y-auto h-[calc(100%-1rem)] max-h-full">
+        <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed inset-0 top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto h-[calc(100%-1rem)] max-h-full">
             <div class="relative w-full max-w-md max-h-full">
                 <!-- Modal content -->
                 <div class="relative bg-white rounded-lg shadow">

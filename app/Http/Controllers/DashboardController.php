f873586ac6272b9e7b3d3d154be98c2c7aeb6bdd
irfanfajru <irfanfajru025@gmail.com>
2023-07-23 16:16:19 +0700

    filter dashboard admin updated prodi

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    function viewDashboard(Request $request){
+    function viewDashboard(Request $request)
+    {
         $dataMahasiswa = Mahasiswa::get();
 
         return view('admin.dashboardAdmin', compact('dataMahasiswa'));
@@ -35,26 +36,49 @@ class DashboardController extends Controller
         //     $dataMahasiswa = Mahasiswa::where('status_id', '8')->get();
         // }
 
-    
+
         // return view('admin.dashboardAdmin')->with('dataMahasiswa', $dataMahasiswa);
 
         $dataMahasiswa = [];
+        $statusId = null;
         if ($request->status == 'pengajuanJudul') {
             $dataMahasiswa = Mahasiswa::where('status_id', '1')->get();
+            $statusId = 1;
         } elseif ($request->status == 'pengajuanSempro') {
             $dataMahasiswa = Mahasiswa::where('status_id', '2')->get();
+            $statusId = 2;
         } elseif ($request->status == 'lulusSempro') {
             $dataMahasiswa = Mahasiswa::where('status_id', '3')->get();
+            $statusId = 3;
         } elseif ($request->status == 'tidakLulusSempro') {
             $dataMahasiswa = Mahasiswa::where('status_id', '4')->get();
+            $statusId = 4;
         } elseif ($request->status == 'pengajuanSidang') {
             $dataMahasiswa = Mahasiswa::where('status_id', '5')->get();
+            $statusId = 5;
         } elseif ($request->status == 'lulusSidang') {
             $dataMahasiswa = Mahasiswa::where('status_id', '6')->get();
+            $statusId = 6;
         } elseif ($request->status == 'tidakLulusSidang') {
             $dataMahasiswa = Mahasiswa::where('status_id', '7')->get();
+            $statusId = 7;
         } else {
+            $statusId = 8;
             $dataMahasiswa = Mahasiswa::where('status_id', '8')->get();
+        }
+
+        // filter prodi
+        // kalo prodinya di select dan statusnya juga di select
+        if ($request->prodi && $statusId) {
+            $dataMahasiswa = Mahasiswa::where('status_id', $statusId)->Where('prodi', 'like', '%' . $request->prodi . '%')->get();
+        } else if ($request->prodi) {
+            // kalo prodinya doang yg di select
+            // ambil data mahasiswa like prodi
+            $dataMahasiswa = Mahasiswa::Where('prodi', 'like', '%' . $request->prodi . '%')->get();
+        } else if ($request->prodi == null && $request->status == null) {
+            // kalo select prodinya kosong sama statusnya kosong
+            // ambil semua datanya dek
+            $dataMahasiswa = Mahasiswa::all();
         }
 
         return view('admin.dashboardAdmin')->with(['dataMahasiswa' => $dataMahasiswa, 'request' => $request]);
--- resources/views/admin/dashboardAdmin.blade.php
+++ resources/views/admin/dashboardAdmin.blade.php
@@ -1,7 +1,7 @@
 <x-admin-layout :title="'Dashboard Admin'">
     <div class="col-span-2 bg-white rounded-lg shadow-md p-4">
         <h3 class="text-lg font-medium">Filter by :</h3>
-        <form method="post" action="{{ route('admin.dashboard-admin-filter') }}"enctype="multipart/form-data" class="flex flex-col mt-4">
+        <form method="post" action="{{ route('admin.dashboard-admin-filter') }}" enctype="multipart/form-data" class="flex flex-col mt-4">
             @csrf
             <div class="flex justify-between space-x-10">
                 <div class="w-1/2 mb-4">
@@ -19,50 +19,50 @@
                     </select>
                 </div>
                 <div class="w-1/2 mb-4">
-                    <label for="option2" class="block text-sm font-medium text-gray-700">Program Studi</label>
-                    <select id="option2" name="option2" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                    <label for="prodi" class="block text-sm font-medium text-gray-700">Program Studi</label>
+                    <select id="prodi" name="prodi" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                         <option value="">Seluruh Pordi</option>
-                        <option value="option1">Option 1</option>
-                        <option value="option2">Option 2</option>
-                        <option value="option3">Option 3</option>
+                        <option value="Teknik Informatika" {{ request('prodi') == 'Teknik Informatika' ? 'selected' : '' }}>Teknik Informatika</option>
+                        <option value="Teknik Multimedia Digital" {{ request('prodi') == 'Teknik Multimedia Digital' ? 'selected' : '' }}>Teknik Multimedia Digital</option>
                     </select>
                 </div>
                 <div class="">
-                <button type="submit" class="mt-4 w-fit bg-edit hover:bg-hoverEdit hover:bg-font text-white font-bold py-2 px-4 rounded">
-                    Cari
-                </button>
-            </div>
+                    <button type="submit" class="mt-4 w-fit bg-edit hover:bg-hoverEdit hover:bg-font text-white font-bold py-2 px-4 rounded">
+                        Cari
+                    </button>
+                </div>
             </div>
         </form>
     </div>
 
+    <p class="mt-2">Total : {{count($dataMahasiswa)}}</p>
     <!-- component -->
     <table class="min-w-full border-collapse block md:table mt-4">
-		<thead class="block md:table-header-group text-sm">
-			<tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-				<th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Nama</th>
-				<th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">NIM</th>
-				<th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Prodi</th>
-				<th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Kelas</th>
-				<th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Status</th>
+        <thead class="block md:table-header-group text-sm">
+            <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+                <th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Nama</th>
+                <th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">NIM</th>
+                <th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Prodi</th>
+                <th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Kelas</th>
+                <th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Status</th>
                 <th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Dosen Pembimbing</th>
                 <th class="bg-primary p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Judul</th>
             </tr>
-		</thead>
+        </thead>
         <tbody>
-        @forelse($dataMahasiswa as $mahasiswa)
-			<tr class="bg-white border border-grey-500 md:border-none block md:table-row text-sm">
-				<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Nama</span>{{$mahasiswa->nama}}</td>
-				<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">NIM</span>{{$mahasiswa->nim}}</td>
-				<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Prodi</span>{{$mahasiswa->prodi}}</td>
-				<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Kelas</span>{{$mahasiswa->kelas}}</td>
-				<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Status</span>{{$mahasiswa->status->nama}}</td>
+            @forelse($dataMahasiswa as $mahasiswa)
+            <tr class="bg-white border border-grey-500 md:border-none block md:table-row text-sm">
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Nama</span>{{$mahasiswa->nama}}</td>
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">NIM</span>{{$mahasiswa->nim}}</td>
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Prodi</span>{{$mahasiswa->prodi}}</td>
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Kelas</span>{{$mahasiswa->kelas}}</td>
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Status</span>{{$mahasiswa->status->nama}}</td>
                 <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Dosen Pembimbing</span>{{$mahasiswa->dosen ? $mahasiswa->dosen->nama:""}}</td>
                 <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Judul</span>{{$mahasiswa->skripsi ? $mahasiswa->skripsi->judul:""}}</td>
-			</tr>
-        @empty
+            </tr>
+            @empty
             <p class="mt-4">Data mahasiswa tidak tersedia</p>
-        @endforelse
-		</tbody>
-	</table>
+            @endforelse
+        </tbody>
+    </table>
 </x-admin-layout>

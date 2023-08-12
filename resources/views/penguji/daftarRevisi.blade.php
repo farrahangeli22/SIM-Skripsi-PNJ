0baa7d1ca84b4ac1f60987be9c42c9aee9951437
unknown <niyaraarinda3@gmail.com>
2023-08-12 16:33:16 +0700

    create view for user penguji

--- resources/views/penguji/daftarRevisi.blade.php
+++ resources/views/penguji/daftarRevisi.blade.php
@@ -1,5 +1,26 @@
 <x-penguji-layout :title="'Daftar Revisi Skripsi'">
 
-    ini daftar revisi
-
+<h1 class="font-bold mt-11">Daftar Persetujuan Revisi</h1>
+    <div class="w-full h-full mt-5">
+        <table class="min-w-full border-collapse block md:table">
+            <thead class="block md:table-header-group">
+                <tr class="bg-primary text-white font-bold text-left block md:table-row">
+                    <th class="p-2 md:border md:border-grey-500 block md:table-cell">Tanggal</th>
+                    <th class="p-2 md:border md:border-grey-500 block md:table-cell">Nama</th>
+                    <th class="p-2 md:border md:border-grey-500 block md:table-cell">Status</th>
+                    <th class="p-2 md:border md:border-grey-500 block md:table-cell">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Data mahasiswa yang mengajuan persetujuan revisi akan ditampilkan di sini -->
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Tanggal</span>14 Agustus 2023</td>
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Nama</span>Niyara Arinda</td>
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Status</span>Diterima</td>
+                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                    <span class="inline-block w-1/3 md:hidden font-bold">Aksi</span>
+                    <button class="bg-edit hover:bg-hoverEdit text-white font-bold py-1 px-2 border border-edit rounded">Edit</button>
+                </td>        
+            </tbody>
+        </table>
+    </div>
 </x-penguji-layout>

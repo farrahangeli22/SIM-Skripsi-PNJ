3b306f3af05136ad1bcc90427605b4d9571682e9
unknown <niyaraarinda3@gmail.com>
2023-08-12 17:14:09 +0700

    update wording button

--- resources/views/penguji/daftarRevisi.blade.php
+++ resources/views/penguji/daftarRevisi.blade.php
@@ -18,7 +18,7 @@
                 <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Status</span>Diterima</td>
                 <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                     <span class="inline-block w-1/3 md:hidden font-bold">Aksi</span>
-                    <button class="bg-edit hover:bg-hoverEdit text-white font-bold py-1 px-2 border border-edit rounded">Edit</button>
+                    <button class="bg-edit hover:bg-hoverEdit text-white font-bold py-1 px-2 border border-edit rounded">Aksi</button>
                 </td>        
             </tbody>
         </table>
--- resources/views/penguji/daftarSidang.blade.php
+++ resources/views/penguji/daftarSidang.blade.php
@@ -19,7 +19,7 @@
                 <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Judul</span>Rancang Bangun Sistem Informasi Manajemen Skrpsi Terintegrasi untuk modul sidang skripsi dan penyerahan alat berbasis web</td>    
                 <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                     <span class="inline-block w-1/3 md:hidden font-bold">Aksi</span>
-                    <button class="bg-edit hover:bg-hoverEdit text-white font-bold py-1 px-2 border border-edit rounded">Edit</button>
+                    <button class="bg-edit hover:bg-hoverEdit text-white font-bold py-1 px-2 border border-edit rounded">Aksi</button>
                 </td>        
             </tbody>
         </table>
